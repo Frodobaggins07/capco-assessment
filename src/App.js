22447d7104b2790d3f33c8d44c436d062612c599
SamuelTheDream <ssamuelodez@yahoo.com>
2023-03-19 22:46:52 -0400

    modified:   src/App.js new file:   src/components/AddContact/AddContact.jsx modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	modified:   src/components/AddContact/AddContact.jsx modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ContactList from './components/ContactList/ContactList';
+import AddContact from './components/AddContact/AddContact';
+import EditContact from './components/EditContact/EditContact';
+
+const MockDatabase = [
+  {
+    id: 1,
+    name: 'John Doe',
+    phone: '+14162345678'
+  },
+  {
+    id: 2,
+    name: 'Person 2',
+    phone: '+3434543545'
+  },
+  {
+    id: 3,
+    name: 'Person 3',
+    phone: '+78987978'
+  },
+  {
+    id: 4,
+    name: 'Person 4',
+    phone: '+45435435345'
+  },
+]
+
 
 function App() {
+  const [contacts, setContacts] = useState(MockDatabase);
+  const [edit, setEdit] = useState(undefined);
+
+  const handleNewContact = (contact) => {
+    setContacts([...contacts, contact]);
+  }
+
+  const handleUpdateContact = (contact) => {
+    const index = contacts.findIndex(e => e.id === contact.id)
+
+    const data = contacts;
+    data.splice(index, 1, contact)
+    setContacts([...data])
+
+  }
+
+  const handleDelete = (contact) => {
+    const index = contacts.findIndex(e => e.id === contact.id)
+
+    const data = contacts;
+    data.splice(index, 1,)
+    setContacts([...data])
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <ContactList data={contacts} onEdit={setEdit} onDelete={handleDelete} />
+        {!edit ? (
+          <AddContact onAddContact={handleNewContact} />
+        ) : (
+          <EditContact data={edit} onUpdateContact={handleUpdateContact} onCancel={() => setEdit(undefined)} />
+        )
+        }
+      </div>
     </div>
   );
 }
